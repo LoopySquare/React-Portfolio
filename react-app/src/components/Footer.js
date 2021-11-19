71baade03771b9a27fa9f7562ee2f51647dcf7e1
LoopySquare <tjobin1@gmail.com>
2021-11-19 15:43:11 -0700

    added footer buttons

--- react-app/src/components/Footer.js
+++ react-app/src/components/Footer.js
@@ -1,7 +1,10 @@
 export default function Footer() {
   return (
     <div className="footer">
-      
+      <div>
+        <a href="https://github.com/LoopySquare" target="_blank" className="is-light is-large git-btn"><i className="fab fa-github-square fa-6x" aria-hidden="tue"></i></a>
+        <a href="https://www.linkedin.com/in/tommy-jobin-709a74150/" target="_blank" className="is-light is-large link-btn"><i className="fab fa-linkedin fa-6x" aria-hidden="true"></i></a>
+      </div>
     </div>
   );
 }
--- react-app/src/pages/About.js
+++ react-app/src/pages/About.js
@@ -6,7 +6,7 @@ export default function About() {
       <div className='about-me'>
         <h1 className='about-title'>ABOUT ME</h1>
         <div className='about-text-box'>
-          <p className='about-text'>Hello, my name is Tommy Jobin. I am an aspiring full stack web developer and an experienced graphic designer. I am currently learning HTML, CSS, JavaScript, Node.js, MySQL, and more. As for the design side, I have several years of experience with Adobe Photoshop, Illustrator, and inDesign. When I am not working you can find me playing one of my favorite video games, going for a hike, snowboarding, or watching the Minnesota Vikings attempt to win a football game.</p>
+          <p className='about-text'>Hello, my name is Tommy Jobin. I am an aspiring full stack web developer and an experienced graphic designer. I am currently learning HTML, CSS, JavaScript, React, Node, MySQL, and more. As for the design side, I have several years of experience with Adobe Photoshop, Illustrator, and inDesign. When I am not working you can find me playing one of my favorite video games, going for a hike, snowboarding, or watching the Minnesota Vikings attempt to win a football game.</p>
           <img src={aboutPic} className='about-pic'></img>
         </div>
              
