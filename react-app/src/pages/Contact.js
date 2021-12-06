52e082a7b9065a1ac8008cfc5824af525f837dfd
LoopySquare <tjobin1@gmail.com>
2021-12-06 16:08:17 -0700

    added material ui, added image gallery to designer projects, added contact page

--- react-app/src/pages/Contact.js
+++ react-app/src/pages/Contact.js
@@ -4,10 +4,63 @@ function Contact() {
     
   
     return (
-      <div className="contact">
-        <h2>Contact Me</h2>
-        
-      </div>
+      <>
+      <h4 className="contact-header">CONTACT ME</h4>
+      <main className="contact">
+          <div className="login-box">
+              <form className='contact-form'>
+              <div className="contact-field">
+                <label className='contact-label' htmlFor="firstName">First Name:</label>
+                <input
+                  className='form-input'
+                  placeholder="First"
+                  name="firstName"
+                  type="firstName"
+                  id="firstName"
+                />
+              </div>
+              <div className="contact-field">
+                <label className='contact-label' htmlFor="lastName">Last Name:</label>
+                <input
+                  className='form-input'
+                  placeholder="Last"
+                  name="lastName"
+                  type="lastName"
+                  id="lastName"
+                />
+              </div>
+              <div className="contact-field">
+                <label className='contact-label' htmlFor="phoneNumber">Phone Number:</label>
+                <input
+                  className='form-input'
+                  placeholder="123-456-7890"
+                  name="phoneNumber"
+                  type="phoneNumber"
+                  id="phoneNumber"
+                />
+              </div>
+              <div className="contact-field">
+                <label className='contact-label' htmlFor="email">Email:</label>
+                <input
+                  className='form-input'
+                  placeholder="youremail@email.com"
+                  name="email"
+                  type="email"
+                  id="email"
+                />
+              </div>
+              <div className="contact-field">
+                <label className='contact-label' htmlFor="password">Message:</label>
+                <textarea rows="4" cols="50" name="comment" form="usrform" className='form-input' placeholder='Enter text here'>
+                </textarea>
+              </div>
+              <div className="signup-btn flex-row space-between">
+                <button className='btn' type="submit">Submit</button>
+              </div>
+            </form>
+          </div>
+      </main>
+      </>
     );
   }
   
--- react-app/src/pages/CreativeProjects.js
+++ react-app/src/pages/CreativeProjects.js
@@ -1,3 +1,6 @@
+import * as React from 'react';
+import ImageList from '@mui/material/ImageList';
+import ImageListItem from '@mui/material/ImageListItem';
 import one from '../img/edits/1.jpg'
 import two from '../img/edits/2.jpg'
 import three from '../img/edits/3.png'
@@ -33,86 +36,282 @@ import twentynine from '../img/edits/29.jpg'
 import thirty from '../img/edits/30.png'
 import thirtyone from '../img/edits/31.png'
 
-export default function CreativeProjects() {
+function srcset(image, size, rows = 1, cols = 1) {
+  return {
+    src: `${image}?w=${size * cols}&h=${size * rows}&fit=crop&auto=format`,
+    srcSet: `${image}?w=${size * cols}&h=${
+      size * rows
+    }&fit=crop&auto=format&dpr=2 2x`,
+  };
+}
+
+export default function QuiltedImageList() {
+  return (
+    <>
+    <h2 className='designer-header'>DESIGNER PROJECTS</h2>
+    <div className='gallery'>
+    <ImageList
+      sx={{ width: 'auto', height: 'auto' }}
+      variant="quilted"
+      cols={4}
+      rowHeight={521}
+    >
+      {itemData.map((item) => (
+        <ImageListItem key={item.img} cols={item.cols || 2} rows={item.rows || 1}>
+          <img
+            {...srcset(item.img, 121, item.rows, item.cols)}
+            alt={item.title}
+            loading="lazy" 
+            className='design-photo'
+          >
+          </img>
+        </ImageListItem>
+      ))}
+    </ImageList>
+    </div>
+    </>
+  );
+}
+
+
+// export default function WovenImageList() {
+//   return (
+//     <>
+//     <h2 className='designer-header'>Designer Projects</h2>
+//     <ImageList sx={{ width: 1500, height: "auto" }} variant="woven" cols={3} gap={8} className="gallery">
+//       {itemData.map((item) => (
+//         <ImageListItem key={item.img}>
+//           <img
+//             src={`${item.img}?w=161&fit=crop&auto=format`}
+//             srcSet={`${item.img}?w=161&fit=crop&auto=format&dpr=2 2x`}
+//             alt={item.title}
+//             loading="lazy"
+//             className='creative-img'
+//           />
+//         </ImageListItem>
+//       ))}
+//     </ImageList>
+//     </>
+//   );
+// }
+
+
+
+
+const itemData = [
+  {
+    img: one,
+  },
+  {
+    img: two,
+  },
+  {
+    img: three,
+  },
+  {
+    img: four,
+  },
+  {
+    img: five,
+  },
+  {
+    img: six,
+  },
+  {
+    img: seven,
+  },
+  {
+    img: eight,
+  },
+  {
+    img: nine,
+  },
+  {
+    img: ten,
+  },
+  {
+    img: eleven,
+  },
+  {
+    img: twelve,
+  },
+  {
+    img: thirteen,
+  },
+  {
+    img: fourteen,
+  },
+  {
+    img: fifteen,
+  },
+  {
+    img: sixteen,
+  },
+  {
+    img: seventeen,
+  },
+  {
+    img: seventeen2,
+  },
+  {
+    img: eighteen,
+  },
+  {
+    img: nineteen,
+  },
+  {
+    img: twenty,
+  },
+  {
+    img: twentyone,
+  },
+  {
+    img: twentytwo,
+  },
+  {
+    img: twentythree,
+  },
+  {
+    img: twentyfour,
+  },
+  {
+    img: twentyfive,
+  },
+  {
+    img: twentysix,
+  },
+  {
+    img: twentyseven,
+  },
+  {
+    img: twentyseven1,
+  },
+  {
+    img: twentyseven2,
+  },
+  {
+    img: twentyeight,
+  },
+  {
+    img: twentynine,
+  },
+  {
+    img: thirty,
+  },
+  {
+    img: thirtyone,
+  },
+
   
-    return (
-      <div>
-          <h1>CREATIVE PROJECTS</h1>
-          <div className="row edits">
-            <div className="col-lg-4 col-md-12 mb-4 mb-lg-0">
-                <img src={one} className="w-100 shadow-1-strong rounded mb-4" alt=""/>
+];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// export default function CreativeProjects() {
+  
+//     return (
+//       <div>
+//           <h1>CREATIVE PROJECTS</h1>
+//           <div className="row edits">
+//             <div className="col-lg-4 col-md-12 mb-4 mb-lg-0">
+//                 <img src={one} className="w-100 shadow-1-strong rounded mb-4" alt=""/>
     
-                <img src={two} className="w-100 shadow-1-strong rounded mb-4" alt=""/>
+//                 <img src={two} className="w-100 shadow-1-strong rounded mb-4" alt=""/>
     
-                <img src={three} className="w-100 shadow-1-strong rounded mb-4" alt=""/>
+//                 <img src={three} className="w-100 shadow-1-strong rounded mb-4" alt=""/>
     
-                <img src={four} className="w-100 shadow-1-strong rounded mb-4" alt=""/>
+//                 <img src={four} className="w-100 shadow-1-strong rounded mb-4" alt=""/>
     
-                <img src={five} className="w-100 shadow-1-strong rounded mb-4" alt=""/>
+//                 <img src={five} className="w-100 shadow-1-strong rounded mb-4" alt=""/>
     
-                <img src={six} className="w-100 shadow-1-strong rounded mb-4" alt=""/>
+//                 <img src={six} className="w-100 shadow-1-strong rounded mb-4" alt=""/>
     
-                <img src={seven} className="w-100 shadow-1-strong rounded mb-4" alt=""/>
+//                 <img src={seven} className="w-100 shadow-1-strong rounded mb-4" alt=""/>
     
-                <img src={eight} className="w-100 shadow-1-strong rounded mb-4" alt=""/>
+//                 <img src={eight} className="w-100 shadow-1-strong rounded mb-4" alt=""/>
     
-                <img src={nine} className="w-100 shadow-1-strong rounded mb-4" alt=""/>
+//                 <img src={nine} className="w-100 shadow-1-strong rounded mb-4" alt=""/>
     
-                <img src={ten} className="w-100 shadow-1-strong rounded mb-4" alt=""/>
+//                 <img src={ten} className="w-100 shadow-1-strong rounded mb-4" alt=""/>
     
-                <img src={eleven} className="w-100 shadow-1-strong rounded mb-4" alt=""/>
+//                 <img src={eleven} className="w-100 shadow-1-strong rounded mb-4" alt=""/>
     
-                <img src={twelve} className="w-100 shadow-1-strong rounded mb-4" alt=""/>
-            </div>
+//                 <img src={twelve} className="w-100 shadow-1-strong rounded mb-4" alt=""/>
+//             </div>
           
-            <div className="col-lg-4 mb-4 mb-lg-0">
-              <img src={thirteen} className="w-100 shadow-1-strong rounded mb-4" alt=""/>
+//             <div className="col-lg-4 mb-4 mb-lg-0">
+//               <img src={thirteen} className="w-100 shadow-1-strong rounded mb-4" alt=""/>
     
-              <img src={fourteen} className="w-100 shadow-1-strong rounded mb-4" alt=""/>
+//               <img src={fourteen} className="w-100 shadow-1-strong rounded mb-4" alt=""/>
     
-              <img src={fifteen} className="w-100 shadow-1-strong rounded mb-4" alt=""/>
+//               <img src={fifteen} className="w-100 shadow-1-strong rounded mb-4" alt=""/>
     
-              <img src={sixteen} className="w-100 shadow-1-strong rounded mb-4" alt=""/>
+//               <img src={sixteen} className="w-100 shadow-1-strong rounded mb-4" alt=""/>
     
-              <img src={seventeen} className="w-100 shadow-1-strong rounded mb-4" alt=""/>
+//               <img src={seventeen} className="w-100 shadow-1-strong rounded mb-4" alt=""/>
     
-              <img src={seventeen2} className="w-100 shadow-1-strong rounded mb-4" alt=""/>
+//               <img src={seventeen2} className="w-100 shadow-1-strong rounded mb-4" alt=""/>
     
-              <img src={eighteen} className="w-100 rounded mb-4" alt=""/>
+//               <img src={eighteen} className="w-100 rounded mb-4" alt=""/>
     
-              <img src={nineteen} className="w-100 rounded mb-4" alt=""/>
+//               <img src={nineteen} className="w-100 rounded mb-4" alt=""/>
     
-              <img src={twenty} className="w-100 rounded mb-4" alt=""/>
-            </div>
+//               <img src={twenty} className="w-100 rounded mb-4" alt=""/>
+//             </div>
           
-            <div className="col-lg-4 mb-4 mb-lg-0">
-                <img src={twentyone} className="w-100 shadow-1-strong rounded mb-4" alt=""/>
+//             <div className="col-lg-4 mb-4 mb-lg-0">
+//                 <img src={twentyone} className="w-100 shadow-1-strong rounded mb-4" alt=""/>
     
-              <img src={twentytwo} className="w-100 shadow-1-strong rounded mb-4" alt=""/>
+//               <img src={twentytwo} className="w-100 shadow-1-strong rounded mb-4" alt=""/>
           
-              <img src={twentythree} className="w-100 shadow-1-strong rounded mb-4" alt=""/>
+//               <img src={twentythree} className="w-100 shadow-1-strong rounded mb-4" alt=""/>
     
-              <img src={twentyfour} className="w-100 shadow-1-strong rounded mb-4" alt=""/>
+//               <img src={twentyfour} className="w-100 shadow-1-strong rounded mb-4" alt=""/>
     
-              <img src={twentyfive} className="w-100 shadow-1-strong rounded mb-4" alt=""/>
+//               <img src={twentyfive} className="w-100 shadow-1-strong rounded mb-4" alt=""/>
               
-              <img src={twentysix} className="w-100 shadow-1-strong rounded mb-4" alt=""/>
+//               <img src={twentysix} className="w-100 shadow-1-strong rounded mb-4" alt=""/>
     
-              <img src={twentyseven} className="w-100 shadow-1-strong rounded mb-4" alt=""/>
+//               <img src={twentyseven} className="w-100 shadow-1-strong rounded mb-4" alt=""/>
     
-              <img src={twentyseven1} className="w-100 shadow-1-strong rounded mb-4" alt=""/>
+//               <img src={twentyseven1} className="w-100 shadow-1-strong rounded mb-4" alt=""/>
     
-              <img src={twentyseven2} className="w-100 shadow-1-strong rounded mb-4" alt=""/>
+//               <img src={twentyseven2} className="w-100 shadow-1-strong rounded mb-4" alt=""/>
     
-              <img src={twentyeight} className="w-100 shadow-1-strong rounded mb-4" alt=""/>
+//               <img src={twentyeight} className="w-100 shadow-1-strong rounded mb-4" alt=""/>
     
-              <img src={twentynine} className="w-100 shadow-1-strong rounded mb-4" alt=""/>
+//               <img src={twentynine} className="w-100 shadow-1-strong rounded mb-4" alt=""/>
     
-              <img src={thirty} className="w-100 shadow-1-strong rounded mb-4" alt=""/>
+//               <img src={thirty} className="w-100 shadow-1-strong rounded mb-4" alt=""/>
     
-              <img src={thirtyone} className="w-100 shadow-1-strong rounded mb-4" alt=""/>
-            </div>
-        </div>
-      </div>
-    );
-  }
+//               <img src={thirtyone} className="w-100 shadow-1-strong rounded mb-4" alt=""/>
+//             </div>
+//         </div>
+//       </div>
+//     );
+//   }
